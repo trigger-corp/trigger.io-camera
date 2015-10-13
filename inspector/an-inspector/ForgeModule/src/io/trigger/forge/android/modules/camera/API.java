76902d0f08e4daf2bcfefe65ca4ecbd26dd46f1c
Antoine van Gelder <antoine@trigger.io>
2015-10-13 15:34:24 +0200

    Support runtime permissions on Android 6.0

--- inspector/an-inspector/ForgeModule/src/io/trigger/forge/android/modules/camera/API.java
+++ inspector/an-inspector/ForgeModule/src/io/trigger/forge/android/modules/camera/API.java
@@ -3,13 +3,22 @@ package io.trigger.forge.android.modules.camera;
 import io.trigger.forge.android.core.ForgeApp;
 import io.trigger.forge.android.core.ForgeFile;
 import io.trigger.forge.android.core.ForgeTask;
+import io.trigger.forge.android.modules.camera.EventListener;
 
 public class API {
 	public static void getImage(final ForgeTask task) {
+		if (!EventListener.checkPermissions()) {
+			task.error("Permission denied", "UNEXPECTED_FAILURE", null);
+			return;
+		}
 		ModalView modal = new ModalView();
 		modal.openModal(task);
 	}
 	public static void URL(final ForgeTask task) {
+		if (!EventListener.checkPermissions()) {
+			task.error("Permission denied", "UNEXPECTED_FAILURE", null);
+			return;
+		}
 		task.success(new ForgeFile(ForgeApp.getActivity(), task.params).url());
 	}
 }
