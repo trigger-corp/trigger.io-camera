12fe22869d9eb0fb5683a4890621dd9c94ff5d79
Antoine van Gelder <antoine@trigger.io>
2016-01-28 16:20:42 +0200

    Only present app permissions dialog when module methods are invoked.

--- inspector/an-inspector/ForgeModule/src/io/trigger/forge/android/modules/camera/API.java
+++ inspector/an-inspector/ForgeModule/src/io/trigger/forge/android/modules/camera/API.java
@@ -1,24 +1,46 @@
 package io.trigger.forge.android.modules.camera;
 
+import android.Manifest;
+
+import io.trigger.forge.android.core.ForgeActivity;
 import io.trigger.forge.android.core.ForgeApp;
 import io.trigger.forge.android.core.ForgeFile;
 import io.trigger.forge.android.core.ForgeTask;
-import io.trigger.forge.android.modules.camera.EventListener;
 
 public class API {
 	public static void getImage(final ForgeTask task) {
-		if (!EventListener.checkPermissions()) {
-			task.error("Permission denied", "UNEXPECTED_FAILURE", null);
-			return;
-		}
-		ModalView modal = new ModalView();
-		modal.openModal(task);
+		ForgeApp.getActivity().requestPermission(Manifest.permission.CAMERA, new ForgeActivity.EventAccessBlock() {
+			@Override
+			public void run(boolean granted) {
+				if (!granted) {
+					task.error("Permission denied. User didn't grant access to camera.", "EXPECTED_FAILURE", null);
+					return;
+				}
+				ForgeApp.getActivity().requestPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE, new ForgeActivity.EventAccessBlock() {
+					@Override
+					public void run(boolean granted) {
+						if (!granted) {
+							task.error("Permission denied. User didn't grant access to filesystem.", "EXPECTED_FAILURE", null);
+							return;
+						}
+						ModalView modal = new ModalView();
+						modal.openModal(task);
+					}
+				});
+			}
+		});
 	}
+
 	public static void URL(final ForgeTask task) {
-		if (!EventListener.checkPermissions()) {
-			task.error("Permission denied", "UNEXPECTED_FAILURE", null);
-			return;
-		}
-		task.success(new ForgeFile(ForgeApp.getActivity(), task.params).url());
+		ForgeApp.getActivity().requestPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE, new ForgeActivity.EventAccessBlock() {
+			@Override
+			public void run(boolean granted) {
+				if (!granted) {
+					task.error("Permission denied. User didn't grant access to filesystem.", "EXPECTED_FAILURE", null);
+					return;
+				}
+				task.success(new ForgeFile(ForgeApp.getActivity(), task.params).url());
+			}
+		});
 	}
 }
--- inspector/an-inspector/ForgeModule/src/io/trigger/forge/android/modules/camera/EventListener.java
+++ inspector/an-inspector/ForgeModule/src/io/trigger/forge/android/modules/camera/EventListener.java
@@ -3,32 +3,9 @@ package io.trigger.forge.android.modules.camera;
 import io.trigger.forge.android.core.ForgeApp;
 import io.trigger.forge.android.core.ForgeEventListener;
 import io.trigger.forge.android.core.ForgeLog;
-import android.Manifest;
-import android.content.pm.PackageManager;
-import android.os.Bundle;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.view.KeyEvent;
 
 public class EventListener extends ForgeEventListener {
-	
-static final int PERMISSIONS_REQUEST = 1;
-	
-	@Override
-    public void onCreate(Bundle savedInstanceState) {
-		if (!checkPermissions()) {
-			ActivityCompat.requestPermissions(ForgeApp.getActivity(), new String[] {  
-				Manifest.permission.WRITE_EXTERNAL_STORAGE,
-				Manifest.permission.CAMERA
-			}, PERMISSIONS_REQUEST);
-		}
-	}
-	
-	public static boolean checkPermissions() {
-		return ContextCompat.checkSelfPermission(ForgeApp.getActivity(), Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED &&
-			   ContextCompat.checkSelfPermission(ForgeApp.getActivity(), Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED;
-	}
-	
 	@Override
 	public Boolean onKeyDown(int keyCode, KeyEvent event) {
 		if (keyCode == KeyEvent.KEYCODE_BACK && ModalView.lastModal != null && ModalView.lastModal.view != null) {
